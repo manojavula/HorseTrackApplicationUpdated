2f506cf703acf37cc3be8045cb4708a2d26cc8ea
manojavula <manojkumar.avula@gmail.com>
2023-04-05 12:35:17 +0530

    Update InventoryRepository.java

--- HorseTrackApplication/HorseTrackApplication/src/main/java/com/manu/horsetrack/repository/InventoryRepository.java
+++ HorseTrackApplication/HorseTrackApplication/src/main/java/com/manu/horsetrack/repository/InventoryRepository.java
@@ -6,6 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.manu.horsetrack.entity.Inventory;
 
+/**
+ * 
+ * @author manoj
+ * Inventory Repository class to get the list of Inventories and find the inventory by denomination
+ */
 public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
 	
 	  List<Inventory> findAll();
